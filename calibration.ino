97243e35645668a7e60e0183f1534339495215b4
Michael Duarte <47889780+michaelsduarte@users.noreply.github.com>
2021-03-12 18:58:30 -0300

    Add files via upload

---
+++ calibration.ino
@@ -0,0 +1,23 @@
+#include <Servo.h>
+
+#define pinServo1 9
+#define pinPotenciometro A0
+
+Servo servo1;
+
+void setup() {
+  servo.attach(pinServo1);
+  Serial.begin(9600);
+}
+
+void loop() {
+  int lido = analogRead(pinPotenciometro);
+  byte angulo = map(lido, 0, 1023, 0, 179);
+
+  servo.write(angulo);
+
+  Serial.print("A0:");
+  Serial.print(lido);
+  Serial.print(" angulo:");
+  Serial.println(angulo);
+}
